9bb882a5274d66515ff63ba60e63f49e25be572f
lesliefinlayson <84471904+lesliefinlayson@users.noreply.github.com>
2021-09-03 15:26:37 -0700

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+//may not help but adding to see
+var tableData = data;
+
+//this may also not help
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    //Loop through each object in the data and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+
+function handleClick() {
+    //grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //check to see if a date was entered and use it if it was
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+// Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
